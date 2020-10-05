62f8ddf8801c41ab9351bddf86ba83f5b795bc35
Laurens_van_Coeverden <Laurensvanc@hotmail.com>
2020-10-05 02:24:17 +0200

    Hover left+right now works when hoverd on, also added a main page to redirect mobile users

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
-function leftSlider() {
-  console.log("Somone hovers on me!");
-  //   window.scrollBy(50, 0);
-  //   scrolldelay = setTimeout("pageScroll()", 100);
+function leftSlider_start() {
+  window.scrollBy(-50, 0);
+  scrolldelay_left = setTimeout("leftSlider_start()", 100);
+}
+
+function leftSlider_stop() {
+  window.scrollBy(0, 0);
+  clearTimeout(scrolldelay_left);
+}
+
+function rightSlider_start() {
+  window.scrollBy(50, 0);
+  scrolldelay_right = setTimeout("rightSlider_start()", 100);
+}
+
+function rightSlider_stop() {
+  window.scrollBy(0, 0);
+  clearTimeout(scrolldelay_right);
 }
